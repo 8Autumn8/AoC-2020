b6994069ba9c075bcf80890ea1d813e12e38e2b2
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2022-01-23 11:48:33 -0800

    Create Day 2-2.java

---
+++ Day 2-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    int numValidPass = 0;
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < 1000; i++){
+      String input = sc.nextLine();
+      
+      String[] arrOfStr = input.split("[-: ]", 4); 
+      
+      String special_char = arrOfStr[2];
+      int min = Integer.parseInt(arrOfStr[0]) ;
+      int max = Integer.parseInt(arrOfStr[1]);
+      int occurance = 0;
+      String pass = arrOfStr[3]; 
+      System.out.println(Arrays.toString(arrOfStr));
+      if ((pass.substring(min,min+1).equals(special_char) && !pass.substring(max,max+1).equals(special_char)) || (!pass.substring(min,min+1).equals(special_char) && pass.substring(max,max+1).equals(special_char))){
+        numValidPass++;
+      }
+    }
+    System.out.println(numValidPass);
+  }
+}
