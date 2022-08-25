6871e3cc38fe8872c5752b086a7a52a65957e988
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2022-08-24 21:31:26 -0700

    Create Day 14.java

---
+++ Day 14.java
@@ -0,0 +1,165 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;  
+import java.util.*;
+
+class Main {
+  private static List<Integer> values = new ArrayList<Integer>();
+  private static List<Integer> keys = new ArrayList<Integer>();
+  public static void main(String[] args) {
+    HashMap<String,HashMap<Integer,Long>> map = populateArray();
+    //System.out.println(firstStar(map));
+    HashMap<Integer,Long> sums = firstStar(map);
+    long ans = 0;
+    int loopThrough = 0;
+    for (Integer key : sums.keySet()){
+      ans += sums.get(key);
+      loopThrough++;
+    }
+    //System.out.println(ans);
+    //System.out.println(map.size());
+    //System.out.println(sums.size());
+    //System.out.println(sums);
+    //System.out.println(loopThrough);
+    //findDuplicates(values);
+
+  }
+  public static long getDecimal(String binary){  
+    long ans = 0;
+    int position = 0;
+    for (int i = binary.length(); i > 0; i--){
+      if (binary.substring(i-1, i).equals("1")){
+        ans += Math.pow(2,position);
+      }
+      position++;
+    }
+    //System.out.println(ans);
+    
+    return ans;
+  }  
+  public static String getBinary(long decimal){
+    String ans = "";
+     
+    for (int i = 35; i >= 0; i--){
+      if (decimal - Math.pow(2,i) > 0){
+        ans+= "1";
+        decimal-= Math.pow(2,i);
+      } else if (decimal == Math.pow(2,i) && decimal != 0){
+        ans+= "1";
+        decimal-= Math.pow(2,i);
+      } else {
+        ans += "0";
+      }
+
+    }
+
+    return ans;
+  }
+
+  public static HashMap<Integer,Long> firstStar(HashMap<String,HashMap<Integer,Long>> outerMap){
+    HashMap<Integer,Long> sumAns = new HashMap<Integer,Long>();
+    int addedValueTime = 0;
+    long sumWithout = 0;
+    for (String x : outerMap.keySet()){
+      HashMap<Integer,Long> innerMap = outerMap.get(x);
+      for (Integer key : innerMap.keySet()){
+        long value = (innerMap.get(key));
+        String ans = "";
+        //System.out.println(value);
+        String bin = getBinary(value);
+        //System.out.println(bin);
+        //System.out.println(x);
+        for (int i = 0; i < bin.length(); i++){
+          if (bin.substring(i, i+1).equals("0") && x.substring(i, i+1).equals("X")){
+            ans+= "0";
+          } else if (bin.substring(i, i+1).equals("1") && x.substring(i, i+1).equals("1")){
+            ans+= "1";
+          }else if (bin.substring(i, i+1).equals("0") && x.substring(i, i+1).equals("1")){
+            ans+= "1";
+          } else if(bin.substring(i, i+1).equals("1") && x.substring(i, i+1).equals("X")){
+            ans+="1";
+          } else if(bin.substring(i, i+1).equals("") && x.substring(i, i+1).equals("X")){          
+          }else {
+            ans+= "0";
+          }
+        }
+        
+        long temp = getDecimal(ans);
+        //System.out.println("temp: " + temp);
+        innerMap.put(key, temp);
+        sumAns.put(key, temp);
+        values.add(key);  
+        keys.add(key);
+        addedValueTime++;
+        sumWithout+=temp;
+        System.out.println(x + "," + key + "," + bin + "," + ans + "," + temp);
+      }
+      outerMap.put(x,innerMap);
+    }
+    //System.out.println(outerMap);
+   // System.out.println("sums added together without takign into accoutn duplicates: " + sumWithout);
+    //System.out.println("added to the other map: " + addedValueTime);
+    return sumAns;
+  }
+
+  public static HashMap<String,HashMap<Integer,Long>> populateArray(){
+   HashMap<String,HashMap<Integer,Long>> outerMap = new HashMap<String,HashMap<Integer,Long>>();
+
+    try{
+      
+      File myObj = new File("puzzle.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        while (myReader.hasNextLine()) {
+          if (data.contains("mask")){
+            String temp ="";
+            HashMap<Integer,Long> innerMap = new HashMap<Integer,Long>();
+            while (myReader.hasNextLine()) {
+              String innerData = myReader.nextLine();
+              if (innerData.contains("mask")){
+                temp = innerData;
+                break;
+              } else {
+                String[] splitArray = innerData.replaceAll("\\s","").split("[\\[\\]=]");
+                //System.out.println(Arrays.toString(splitArray));
+                innerMap.put(Integer.parseInt(splitArray[1]),Long.parseLong(splitArray[3])); 
+                 
+              }
+            }
+            String[] splitArray = data.replaceAll("\\s","").split("=");
+            outerMap.put(splitArray[1], innerMap);
+            data = temp;
+          }
+        }
+      }
+      myReader.close();
+    } catch (Exception e){
+      System.out.println("error");
+    }
+    //System.out.println(outerMap);
+    //System.out.println("size" + outerMap.size());
+    return outerMap;
+  }
+
+  public static Set<Integer> findDuplicates(List<Integer> listContainingDuplicates)
+  { 
+    final Set<Integer> setToReturn = new HashSet<>(); 
+    final Set<Integer> set1 = new HashSet<>();
+
+    for (Integer yourInt : listContainingDuplicates)
+    {
+    if (!set1.add(yourInt))
+    {
+      setToReturn.add(yourInt);
+    }
+    }
+    //System.out.println("list size: " + listContainingDuplicates.size());
+    //System.out.println("not: " + set1);
+    //Collections.sort(setToReturn);
+    //System.out.println("duplicated values: " + setToReturn);
+    //System.out.println(setToReturn.size());
+    //System.out.println(set1.size());
+    return setToReturn;
+  }
+}
